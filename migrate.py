41c42a6cb2a930145d2ebaccf44caacf4af9c980
Josh Zojonc <Josh.Zojonc@Oregonstate.edu>
2016-06-07 16:07:21 -0700

    Should be working now

--- migrate.py
+++ migrate.py
@@ -11,14 +11,11 @@ with open("input.yml", 'r') as stream:
 for vserver,values  in origcfg.iteritems():
   thiscfg = []
   vip = values['vip']
-  #print values
-
   for port in values['ports']:
     newsg = 'add serviceGroup sg_%s-%s ANY -maxClient 0 -maxReq 0 -cip DISABLED -usip YES -useproxyport NO -cltTimeout 360 -svrTimeout 360 -CKA NO -TCPB YES -CMP NO' %(vserver,port)
     thiscfg.append(newsg)
     newvs = 'add lb vserver lb_%s-%s ANY %s %s -persistenceType NONE -m MAC -state DISABLED -connfailover STATELESS -cltTimeout 120' %(vserver,port,vip,port)
     thiscfg.append(newvs)
-    
     lbsgbind = 'bind lb vserver lb_%s-%s sg_%s-%s' %(vserver,port,vserver,port)
     thiscfg.append(lbsgbind)
   for realserver in values['rs']:
@@ -30,6 +27,11 @@ for vserver,values  in origcfg.iteritems():
       for port in binding:
         sgrsbind = 'bind serviceGroup sg_%s-%s %s %s -state DISABLED' %(vserver,port,rs,port)
         thiscfg.append(sgrsbind)
+  else:
+     for realserver in values['rs']:
+       for port in values['ports']:
+         sgrsbind = 'bind serviceGroup sg_%s-%s %s %s -state DISABLED' %(vserver,port,realserver,port)
+         thiscfg.append(sgrsbind)
   disarp = 'set ns ip %s -arpResponse ALL_VSERVERS' %vip
   thiscfg.append(disarp)
 
